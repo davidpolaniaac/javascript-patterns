4b3454a303338256ce284979d25ea41599b4e0c8
David Polania <davidpolaniaac@users.noreply.github.com>
2019-06-24 09:59:23 -0500

    add new routes with design patterns

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const service = require('./services');
+const handlify = require('./handlers');
 const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+const usersHandler = handlify('users');
+const postsHandler = handlify('posts');
+
+
+app.get('/', usersHandler(service).get);
+app.post('/', usersHandler(service).post);
+app.put('/:id', usersHandler(service).put);
+app.delete('/:id', usersHandler(service).delete);
+
+app.get('/posts', postsHandler(service).get);
+app.post('/posts', postsHandler(service).post);
+app.put('/posts/:id', postsHandler(service).put);
+app.delete('/posts/:id', postsHandler(service).delete);
+
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
